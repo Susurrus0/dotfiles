c426ab7e83e67e4a19af128db1b81c3e121257d5
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-05-16 20:51:33 +0200

    Enable flatpak service

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,6 +109,8 @@
     nerd-fonts.jetbrains-mono
   ];
 
+  services.flatpak.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
