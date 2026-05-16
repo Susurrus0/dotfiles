c8e3ef626acda586576b8f92df9dd632cf5d7441
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-05-16 21:22:27 +0200

    Install Steam

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -90,10 +90,23 @@
 
   programs.firefox.enable = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
+  programs.steam.gamescopeSession.enable = true;
+
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       # Add additional package names here
       "obsidian"
+      "steam"
+      "steam-original"
+      "steam-unwrapped"
+      "steam-run"
     ];
 
   # List packages installed in system profile.
