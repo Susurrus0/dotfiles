60efce58ee666d5e7cea40147fe303200c593a06
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-05-21 00:07:27 +0200

    Add input inheritance for home manager

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -24,6 +24,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = { inherit inputs; };
           home-manager.users.bruno = import ./home.nix;
         }
       ];
