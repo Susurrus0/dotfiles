b1adf72662f268f90cef8e24317ede57bba9899b
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-05-20 23:51:59 +0200

    Add unstable nixpkgs url and specialArgs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,6 +14,7 @@
     nixosConfigurations."ThinkPad-X280" = nixpkgs.lib.nixosSystem {
     # Make sure hostname is correct
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
 
