5aaccadc0009052c2c8ef36abe9f73dab09c093e
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-05-22 14:34:58 +0200

    Rewrite hyprland.nix

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,36 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  wayland.windowManager.hyprland = {
-    enable = true;
+  # write shortcuts to /etc/xdg/hypr/hyprland.conf
+  environment.etc."xdg/hypr/hyprland.conf".text = ''
+    $mainMod = SUPER
 
-    settings = {
-      "$mainMod" = "SUPER";
+    # Basic Launchers
+    bind = $mainMod, Q, exec, kitty
+    bind = $mainMod, C, killactive,
+    bind = $mainMod, M, exit,
+    bind = $mainMod, E, exec, dolphin
+    bind = $mainMod, V, togglefloating,
+    bind = $mainMod, R, exec, wofi --show drun
 
-      # Basic Launchers
-      bind = [
-        "$mainMod, Q, exec, kitty" # Or whatever your preferred terminal emulator is
-        "$mainMod, C, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, E, exec, dolphin" # Launches your Plasma file manager
-        "$mainMod, V, togglefloating,"
-        "$mainMod, R, exec, wofi --show drun" # Application launcher menu
+    # Move focus with mainMod + arrow keys
+    bind = $mainMod, left, movefocus, l
+    bind = $mainMod, right, movefocus, r
+    bind = $mainMod, up, movefocus, u
+    bind = $mainMod, down, movefocus, d
 
-        # Move focus with mainMod + arrow keys
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
-      ];
-
-      monitor = ",preferred,auto,1";
-    };
-  };
-
-  # Essential helper packages specifically for a clean Wayland/Hyprland environment
-  home.packages = with pkgs; [
-    wofi       # An application launcher menu (similar to Plasma's KRunner)
-    waybar     # A highly customizable top status bar for Wayland
-    hyprpaper  # A simple, fast wallpaper utility designed for Hyprland
-  ];
+    monitor = ,1920x1080@60,auto,1
+  '';
 }
