c7d9afb614560ea7fa55c2f565624ba485d88256
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-05-26 13:33:25 +0200

    Swap lock action for hyprlock

--- nixos/home.nix
+++ nixos/home.nix
@@ -137,7 +137,7 @@ in
     layout = [
       {
         label = "lock";
-        action = "loginctl lock-session";
+        action = "hyprlock";
         text = "Lock";
         keybind = "l";
       }
