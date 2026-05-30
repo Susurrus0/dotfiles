b4dd038fdfecc92e0fd35031431de1a31b81af26
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2026-05-30 21:07:51 +0200

    Change NixOS channel from 26.05 to stable

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS with Hyprland";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-stable";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager/release-stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
