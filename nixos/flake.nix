7c875c6c93fbe401b4853600bfb6016f99eca45f
installer <installer@gmail.com>
2026-06-19 22:00:08 +0200

    Add catpuccin theme flake

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -8,9 +8,10 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    catpuccin.url = "github:catppuccin/nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, catpuccin, ... }@inputs: {
     nixosConfigurations."ThinkPad-X280" = nixpkgs.lib.nixosSystem {
     # Make sure hostname is correct
       system = "x86_64-linux";
