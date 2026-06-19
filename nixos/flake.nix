33e22b304d32d82dd378eb8944a9799f2a49f1d0
installer <installer@gmail.com>
2026-06-19 22:08:39 +0200

    Fix typo in catppuccin

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -8,10 +8,10 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    catpuccin.url = "github:catppuccin/nix";
+    catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, catpuccin, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, catppuccin, ... }@inputs: {
     nixosConfigurations."ThinkPad-X280" = nixpkgs.lib.nixosSystem {
     # Make sure hostname is correct
       system = "x86_64-linux";
