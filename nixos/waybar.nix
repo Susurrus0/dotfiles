25e965fb115f5e7694038a80552a4912fb54815c
installer <installer@gmail.com>
2026-06-29 21:22:14 +0200

    Extend the configuration with basic modules

--- nixos/waybar.nix
+++ nixos/waybar.nix
@@ -13,13 +13,32 @@
           "eDP-1"
           "HDMI-A-1"
         ];
-        modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
-        modules-center = [ "sway/window" ];
-        modules-right = [ "battery" "mpd" "temperature" "clock" ];
-
-        "sway/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
+        modules-left = [ "hyprland/workspaces" "wlr/taskbar" ];
+        modules-center = [ "hyprland/window" ];
+        modules-right = [ "pulseaudio" "network" "mpd" "temperature" "battery" "clock" ];
+        "hyprland/workspaces" = {
+          "format" = "{name}: {icon}";
+          "format-icons" = {
+              "1" = "";
+              "2" = "";
+              "3" = "";
+              "4" = "";
+              "5" = "";
+              "active" = "";
+              "default" = "";
+          };
+            "persistent-workspaces" = {
+                  "*" = 5; # 5 workspaces by default on every monitor
+                  # "HDMI-A-1" = 3; # but only three on HDMI-A-1
+            };
+        "battery" = {
+            "format" = "{capacity}% {icon}";
+            "format-icons" = ["" "" "" "" ""];
+        };
+        "clock" = {
+          "interval" = 60;
+          "format" = "{:%I:%M %p}";
+          };
         };
       };
     # style = " ... ";
