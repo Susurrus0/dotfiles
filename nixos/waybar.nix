f967c951d80c8640fae64274cb672a84c0ccb5f8
installer <installer@gmail.com>
2026-06-29 21:47:02 +0200

    Add example style section

--- nixos/waybar.nix
+++ nixos/waybar.nix
@@ -71,7 +71,20 @@
           };
         };
       };
-    # style = " ... ";
+    style = "
+      * {
+        border: none;
+        border-radius: 0;
+        font-family: Source Code Pro;
+      }
+      window#waybar {
+        background: #16191C;
+        color: #AAB2BF;
+      }
+      #workspaces button {
+        padding: 0 5px;
+      }
+    ";
     };
   };
 }
